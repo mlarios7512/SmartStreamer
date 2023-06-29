3fda8c9185d9f6427c4974b0d22bbcb9309eea9a
Michael Larios <lariosmichael98@gmail.com>
2023-06-28 23:31:44 -0700

    Created alert when error occurs saving search item to watchlist. Created controller, view, & basic layout for watchlist.

---
+++ src/SmartStreamers/StreamBudget/ViewModels/WatchlistVM.cs
@@ -0,0 +1,16 @@
+﻿using StreamBudget.Models.DTO.StreamAvail;
+using StreamBudget.Models.Other;
+
+namespace StreamBudget.ViewModels
+{
+    public class WatchlistVM
+    {
+        public IEnumerable<SearchResultDTO> SearchResults { get; set; } = new List<SearchResultDTO>();
+        public List<CompletionTime> CompletionTimes { get; set; } = new List<CompletionTime>();
+
+        public WatchlistVM()
+        {
+
+        }
+    }
+}
---
+++ src/SmartStreamers/StreamBudget/wwwroot/js/searchresults.js
@@ -0,0 +1,53 @@
+﻿$(".add-series-to-watchlist-btn").click(function () {
+    let imdbIdOfSeries = $(this).attr('id').substring(28);
+
+    $.ajax({
+        type: "POST",
+        url: `/api/`,
+        dataType: "json",
+        contentType: "application/json; charset=UTF-8",
+        data: JSON.stringify(imdbIdOfSeries),
+        success: displayItemSavedMsg,
+        error: errorSavingItemMsg
+
+    });
+});
+
+function clearNotification(notificationHtmlID) {
+    $(`#${notificationHtmlID}`).remove();
+}
+
+function notifyUserItemWasAddedToWatchlist(seriesTitle, seriesImdbId) {
+    $(`.watchlist-crud-alert`).remove();
+
+    let alertToDisplay = `
+    <div class="alert alert-info watchlist-crud-alert" id="series-${seriesImdbId}-added-to-watchlist-notification" role="alert">
+        "${seriesTitle}" added to watchlist.
+    </div>`
+        ;
+    $(`body`).append(alertToDisplay);
+
+    const clearAlertTimeout = setTimeout(clearNotification, 3000, `series-${seriesImdbId}-added-to-watchlist-notification`);
+}
+
+
+function displayItemSavedMsg(data) {
+    console.log(`displayItemSavedMsg`);
+
+    //NEED TO VERIFY THAT THIS WORKS!
+  /*  displayItemSavedMsg(data["title"], data["imdbId"]);*/
+}
+
+function errorSavingItemMsg() {
+    console.log(`AJAX error. Item was NOT added to watchlist.`);
+    $(`.watchlist-crud-alert`).remove();
+
+    let alertToDisplay = `
+    <div class="alert alert-danger watchlist-crud-alert" id="error-saving-series-to-watchlist-notification" role="alert">
+        There was an error saving to the item your watchlist.
+    </div>`
+        ;
+    $(`body`).append(alertToDisplay);
+
+    const clearAlertTimeout = setTimeout(clearNotification, 3000, `error-saving-series-to-watchlist-notification`);
+}

9d797c072a31d72b6c695a91ad2acfdfa1b2f89c
Michael Larios <lariosmichael98@gmail.com>
2023-11-23 15:56:28 -0800

    Added visible icon when disabling items for watchtime calc.

---
+++ src/SmartStreamers/StreamBudget/wwwroot/js/ViewWatchllistItems/togglewatchlistitem.js
@@ -0,0 +1,35 @@
+﻿function enableWatchlistItemTowardTotalWatchtimeCalc(togglableItemIconElement) {
+    let showId = togglableItemIconElement.attr('id').substring(36);
+    let curCountedTowardWatchtimeCalc = $(`#total-watchtime-${showId}-input`).attr("name");
+
+    // For some browsers, `attr` is undefined; for others, `attr` is false.  Check for both. 
+    //Taken from the user "strager" here: https://stackoverflow.com/questions/1318076/jquery-hasattr-checking-to-see-if-there-is-an-attribute-on-an-element
+    if (typeof curCountedTowardWatchtimeCalc !== true) {
+        $(`#total-watchtime-${showId}-input`).attr("name", "FullSeriesHoursWatchtime");
+        togglableItemIconElement.removeClass("disabled-togglable-watchlist-item-icon");
+    }
+
+}
+
+function disableWatchlistItemTowardTotalWatchtimeCalc(togglableItemIconElement) {
+    let showId = togglableItemIconElement.attr('id').substring(36);
+    let curCountedTowardWatchtimeCalc = $(`#total-watchtime-${showId}-input`).attr("name");
+
+    if (typeof curCountedTowardWatchtimeCalc !== 'undefined' && curCountedTowardWatchtimeCalc !== false) {
+        $(`#total-watchtime-${showId}-input`).removeAttr("name");
+        togglableItemIconElement.addClass("disabled-togglable-watchlist-item-icon");
+    }
+}
+
+$(".togglable-watchlist-item-icon").click(function () {
+
+    if ($(this).hasClass("disabled-togglable-watchlist-item-icon") == false) {
+        console.log("disabling...");
+        disableWatchlistItemTowardTotalWatchtimeCalc($(this));
+        
+    }
+    else {
+        console.log("enabling...");
+        enableWatchlistItemTowardTotalWatchtimeCalc($(this));
+    }
+});

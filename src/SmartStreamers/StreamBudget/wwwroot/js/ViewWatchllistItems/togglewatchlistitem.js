289e29426bab8cb20f824d6d39bf0f2688b76f02
Michael Larios <lariosmichael98@gmail.com>
2023-12-04 11:52:57 -0800

    Changed visiblity of clickable items. Clarified if items will be excluded from stats.

--- src/SmartStreamers/StreamBudget/wwwroot/js/ViewWatchllistItems/togglewatchlistitem.js
+++ src/SmartStreamers/StreamBudget/wwwroot/js/ViewWatchllistItems/togglewatchlistitem.js
@@ -2,34 +2,32 @@
     let showId = togglableItemIconElement.attr('id').substring(36);
     let curCountedTowardWatchtimeCalc = $(`#total-watchtime-${showId}-input`).attr("name");
 
-    // For some browsers, `attr` is undefined; for others, `attr` is false.  Check for both. 
-    //Taken from the user "strager" here: https://stackoverflow.com/questions/1318076/jquery-hasattr-checking-to-see-if-there-is-an-attribute-on-an-element
     if (typeof curCountedTowardWatchtimeCalc !== true) {
         $(`#total-watchtime-${showId}-input`).attr("name", "FullSeriesHoursWatchtime");
-        togglableItemIconElement.removeClass("disabled-togglable-watchlist-item-icon");
+        $(`#toggle-watchtime-inclusion-btn-item-${showId}`).text("No");
+        togglableItemIconElement.removeClass("excluded-from-watchlist-stats");
     }
-
 }
 
 function disableWatchlistItemTowardTotalWatchtimeCalc(togglableItemIconElement) {
     let showId = togglableItemIconElement.attr('id').substring(36);
     let curCountedTowardWatchtimeCalc = $(`#total-watchtime-${showId}-input`).attr("name");
 
+    // For some browsers, `attr` is undefined; for others, `attr` is false.  Check for both.
+    //Taken from the user "strager" here: https://stackoverflow.com/questions/1318076/jquery-hasattr-checking-to-see-if-there-is-an-attribute-on-an-element
     if (typeof curCountedTowardWatchtimeCalc !== 'undefined' && curCountedTowardWatchtimeCalc !== false) {
         $(`#total-watchtime-${showId}-input`).removeAttr("name");
-        togglableItemIconElement.addClass("disabled-togglable-watchlist-item-icon");
+        $(`#toggle-watchtime-inclusion-btn-item-${showId}`).text("Yes");
+        togglableItemIconElement.addClass("excluded-from-watchlist-stats");
     }
 }
 
-$(".togglable-watchlist-item-icon").click(function () {
+$(".togglable-watchlist-item-indication").click(function () {
 
-    if ($(this).hasClass("disabled-togglable-watchlist-item-icon") == false) {
-        console.log("disabling...");
+    if ($(this).hasClass("excluded-from-watchlist-stats") == false) {
         disableWatchlistItemTowardTotalWatchtimeCalc($(this));
-        
     }
     else {
-        console.log("enabling...");
         enableWatchlistItemTowardTotalWatchtimeCalc($(this));
     }
 });

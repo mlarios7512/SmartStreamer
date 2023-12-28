d016941e1f5ca785081e562de2f0757b830b3695
Michael Larios <lariosmichael98@gmail.com>
2023-12-28 15:47:14 -0800

    Updated XBML documentation for method.

--- src/SmartStreamers/StreamBudget/Services/Abstract/IStreamAvailService.cs
+++ src/SmartStreamers/StreamBudget/Services/Abstract/IStreamAvailService.cs
@@ -5,12 +5,12 @@ namespace StreamBudget.Services.Abstract
     public interface IStreamAvailService
     {
         /// <summary>
-        /// Get search results for a given TV series ONLY. (No movies.
+        /// Get search results for a given TV series ONLY. (
         /// Only returns TV series available in the US that have English listed
         /// as a language option.)
         /// </summary>
-        /// <param name="titleName">The name of the TV series to search for.</param>
-        /// <returns>A list of search results. Returns an empty list if no search results are founds.</returns>
+        /// <param name="titleName">The name of the TV series to search for. Requires 1-100 chars. Allowed input: alphabet chars, "!", ";", and ":".</param>
+        /// <returns>A list of search results. Returns an empty list if no search results are found or if string fails regex verification.</returns>
         public Task<IEnumerable<SearchResultDTO>> GetBasicSearch(string titleName);
 
         /// <summary>
